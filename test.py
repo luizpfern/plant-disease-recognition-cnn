5b9a151ad88be813e307e9f1a5f3f758e9fc4fac
Luiz <luizpfern14@gmail.com>
2025-05-14 23:31:17 -0300

    Atualiza dimensões da imagem e modelo utilizado na função de teste

--- test.py
+++ test.py
@@ -2,20 +2,20 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
-IMG_WIDTH = 150
-IMG_HEIGHT = 150
+IMG_WIDTH = 224
+IMG_HEIGHT = 224
 
 def test_image(image_path):
-    model = tf.keras.models.load_model("models/model_v1.h5")  # Carrega o modelo treinado
+    model = tf.keras.models.load_model("models/model.h5")  # Carrega o modelo treinado
     img = load_img(image_path, target_size=(IMG_WIDTH, IMG_HEIGHT))
     img_array = img_to_array(img) / 255.0  # Normaliza
     img_array = np.expand_dims(img_array, axis=0)
 
     prediction = model.predict(img_array)
-    class_names = ["Saudável", "Doença fúngica", "Seca"]
+    print("prediction:", np.argmax(prediction))
+    class_names = ["Saudável", "Seca"]
     print(f"Arquivo: {image_path} - Predição: {class_names[np.argmax(prediction)]}")
 
 
-test_image("dataset/test/teste.jpg")
-test_image("dataset/test/teste1.jpg")
-test_image("dataset/test/teste2.jpg")
+test_image("dataset/test/test1.jpg")
+test_image("dataset/test/test2.jpg")
